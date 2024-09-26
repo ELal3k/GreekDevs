c29c2404331424d0e9282d4d4ae1a33092cfd4b1
Alexandros Polymeros <alpolymer@gmail.com>
2024-09-26 11:52:54 +0300

    create Article done

--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,13 @@ const connectDB = require("./config/db")
 const app = express()
 const port = process.env.PORT || 5000
 
+const articleRoutes = require("./routes/articleRoute")
+
 connectDB()
 
 app.use(cors())
 app.use(express.json())
+app.use("/api/articles", articleRoutes)
 
 app.get("/", (req, res) => {
   res.send("API is running")
