4a08d25ff0b66d2cbbb53c6cf0056c631cfe4ed7
Alexandros Polymeros <alpolymer@gmail.com>
2024-10-14 11:29:34 +0300

    user controller done

---
+++ backend/controllers/authController.js
@@ -0,0 +1,11 @@
+const User = require("../models/userModel")
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+
+const registerUser = async (req, res) => {}
+
+const loginUser = async (req, res) => {}
+
+const getUserData = async (req, res) => {}
+
+module.exports = { registerUser, loginUser, getUserData }
---
+++ backend/controllers/userController.js
@@ -0,0 +1,38 @@
+const User = require("../models/userModel")
+const bcrypt = require("bcrypt")
+
+const createUser = async (req, res) => {
+  try {
+    const { username, email, password } = req.body
+
+    if (!username || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" })
+    }
+
+    const emailExists = await User.findOne({ email })
+    if (emailExists) {
+      return res.status(400).json({ message: "Email already in use" })
+    }
+
+    const usernameExists = await User.findOne({ username })
+    if (usernameExists) {
+      return res.status(400).json({ message: "Username already in use" })
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    const newUser = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+    })
+
+    return res
+      .status(200)
+      .json({ success: true, message: "User successfully created" })
+  } catch (err) {
+    console.error("Error creating user:", err)
+    return res.status(400).json({ success: false, msg: err })
+  }
+}
