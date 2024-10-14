e979d44416d3ff02f45891cf56e14c42600b8030
Alexandros Polymeros <alpolymer@gmail.com>
2024-10-14 22:15:23 +0300

    middleware done

---
+++ backend/middleware/auth.js
@@ -0,0 +1,35 @@
+const jwt = require("jsonwebtoken")
+
+const secret = process.env.JWT_SECRET
+
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers.authorization
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return req
+      .status(401)
+      .json({ success: false, message: "No token provided" })
+  }
+
+  const token = authHeader.split(" ")[1]
+
+  try {
+    const decoded = jwt.verify(token, secret)
+    req.user = decoded.user
+    next()
+  } catch (err) {
+    if (err instanceof jwt.TokenExpiredError) {
+      return res.status(401).json({ success: false, message: "Token expired" })
+    }
+
+    if (err instanceof jwt.JsonWebTokenError) {
+      return res.status(401).json({ success: false, message: "Invalid token" })
+    }
+
+    return req
+      .status(500)
+      .json({ success: false, message: "Internal server error" })
+  }
+}
+
+module.exports = verifyToken
