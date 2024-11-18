5850207ed348655188bfd14d71479189fc8b2cca
Alexandros Polymeros <alpolymer@gmail.com>
2024-11-18 12:49:03 +0200

    added title input

--- client/src/components/TipTap/tipTapUpdater.jsx
+++ client/src/components/TipTap/tipTapUpdater.jsx
@@ -1,3 +1,45 @@
-export default function TipTapUpdater() {
-  return <div>tipTapUpdater</div>
+import { EditorContent, useEditor } from "@tiptap/react"
+import StarterKit from "@tiptap/starter-kit"
+import Highlight from "@tiptap/extension-highlight"
+import Typography from "@tiptap/extension-typography"
+import CodeBlockLowlight from "@tiptap/extension-code-block-lowlight"
+import { common, createLowlight } from "lowlight"
+import "highlight.js/styles/github-dark-dimmed.css"
+
+import MenuBar from "./menuBar"
+
+const lowlight = createLowlight(common)
+
+export default function TipTapUpdater({ initialContent, onUpdate }) {
+  const editor = useEditor({
+    extensions: [
+      StarterKit.configure({
+        codeBlock: false,
+      }),
+      Highlight,
+      Typography,
+      CodeBlockLowlight.configure({
+        lowlight,
+        languageClassPrefix: "language-",
+      }),
+    ],
+    content: initialContent,
+    editorProps: {
+      attributes: {
+        class: "prose prose-base max-w-none focus:outline-none p-4",
+      },
+    },
+    onUpdate: ({ editor }) => {
+      const html = editor.getHTML
+      onUpdate?.({ html })
+    },
+  })
+  return (
+    <div className="w-full max-w-4xl mx-auto space-y-4 mt-10">
+      <div className="border rounded-lg">
+        <MenuBar editor={editor} />
+        <EditorContent editor={editor} className="bg-white" />
+      </div>
+    </div>
+  )
 }
--- client/src/pages/articleEditForm.jsx
+++ client/src/pages/articleEditForm.jsx
@@ -1,19 +1,28 @@
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
 import useApi from "../hooks/useApi"
 import LoadingSpinner from "../components/UI/loadingSpinner"
-import { useParams } from "react-router-dom"
-import { useEffect } from "react"
+import TipTapUpdater from "../components/TipTap/tipTapUpdater"
+import { toast, ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
 
 export default function ArticleEditForm() {
   const { articleId } = useParams()
   const { response: article, isLoading, fetchData } = useApi()
 
+  const [title, setTitle] = useState("")
+
   useEffect(() => {
     const getArticle = async () => {
       try {
-        fetchData({
+        const res = await fetchData({
           url: `/articles/get/${articleId}`,
           method: "GET",
         })
+
+        if (res) {
+          setTitle(res?.title)
+        }
       } catch (err) {
         console.log("Failed to fetch article", err)
       }
@@ -22,9 +31,38 @@ export default function ArticleEditForm() {
     getArticle()
   }, [articleId])
 
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+  }
+
   if (isLoading) return <LoadingSpinner />
 
-  console.log(article)
-
-  return <div>articleEditForm</div>
+  return (
+    <form onSubmit={handleSubmit} className="max-w-4xl mx-auto p-4">
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+      <h1 className="text-2xl font-bold mb-6">Edit Article</h1>
+      <div className="space-y-4">
+        <input
+          type="text"
+          name="title"
+          id="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          className="w-full px-3 py-4 shadow-md rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 text-4xl font-extrabold text-gray-600"
+          placeholder="Enter article title"
+          disabled={isLoading}
+        />
+      </div>
+    </form>
+  )
 }
